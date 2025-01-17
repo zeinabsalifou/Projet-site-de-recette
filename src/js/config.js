78369c6a34e88b77eab189577f0b37efa286af2d
zeinabsalifou <zeinabsalifou22@gmail.com>
2025-01-16 23:16:39 -0500

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fe01e238-a53c-404a-bd29-67d8e055c97e';
+export const MODAL_CLOSE_SEC = 2.5;
